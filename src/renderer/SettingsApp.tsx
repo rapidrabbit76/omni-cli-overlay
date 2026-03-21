b5e5bb2e0f9bd94bccb426e157888a24be727800
yslee <yslee.dev@gmail.com>
2026-03-22 01:29:41 +0900

    Make settings model options reactive

--- src/renderer/SettingsApp.tsx
+++ src/renderer/SettingsApp.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import SettingsPage from './components/SettingsPage'
 import { getColors, syncTokensToCss, useThemeStore } from './theme'
 import { useSessionStore } from './stores/sessionStore'
@@ -7,6 +7,7 @@ export default function SettingsApp() {
   const isDark = useThemeStore((s) => s.isDark)
   const setSystemTheme = useThemeStore((s) => s.setSystemTheme)
   const initStaticInfo = useSessionStore((s) => s.initStaticInfo)
+  const fetchModels = useSessionStore((s) => s.fetchModels)
 
   useEffect(() => {
     syncTokensToCss(getColors(isDark))
@@ -26,7 +27,8 @@ export default function SettingsApp() {
 
   useEffect(() => {
     void initStaticInfo()
-  }, [initStaticInfo])
+    void fetchModels()
+  }, [fetchModels, initStaticInfo])
 
   return <SettingsPage />
 }
